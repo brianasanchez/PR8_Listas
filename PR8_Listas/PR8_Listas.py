679c5fe88f75ca29d95079e207e0845bcd9e971d
brianasanchez <a.23308051281213@cbtis128.edu.mx>
2024-09-25 19:55:50 -0600

    Add files via upload

---
+++ PR8_Listas/PR8_Listas.py
@@ -0,0 +1,41 @@
+print(" ") #Print imprime un espacio
+
+print("Briana Sarahi Sanchez Perez 3-W, PR8_Listas")
+print(" ") #Print imprime un espacio
+
+#Practica 8  donde usamos listas
+thislist = ["apple", "banana", "cherry"] #Da valores a la lista
+print(thislist) # Muestra el miembro de la lista
+
+#cuando usamos (len( )) te despliega
+#el numero de elementos de la lista
+thislist2 = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
+#Separa con un espacio
+print(" ")
+#Da valores a varias listas
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list4 = ["abc", 34, True, 40, "male"]
+
+#Muestra los miembros de cada lista
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+
+
+print(" ") # Printi imprime un espacio
+
+thislist = ["apple", "banana", "cherry"] # Da valores a la lista
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[-1])
+
+print(" ") # Print imprime un espacio
